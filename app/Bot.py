8b3f9c735b051aa14cb59520fd64e8de43090a9e
Alexander Brovchenko <a.brovchenko@unix4all.com>
2023-02-03 12:34:41 +0200

    change function get_search_news added data time publisher

--- app/Bot.py
+++ app/Bot.py
@@ -1,4 +1,3 @@
-import telebot
 from telebot import types
 import re
 import requests
@@ -12,42 +11,57 @@ from selenium.webdriver.chrome.options import Options
 import datetime
 import telebot
 
+def get_check_news(value):
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-def get_check_user(value):
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='telebot',
-                                 password='123321',
-                                 database='telebot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
     with connection:
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM `Users` WHERE `id` = '{}'".format(value[0])
-            result = cursor.execute(sql)
+            # Read all record
+            sql = "SELECT `Date`,`Tags` FROM `News` WHERE `Tags` = '{}'".format(value)
+            cursor.execute(sql)
             result = cursor.fetchall()
+
             if result:
-                result[0]['Tags'] = f"{result[0]['Tags']}, {value[-1]}"
-                sql = "UPDATE `Users` SET `id`='{}',`Name`='{}',`Tags`='{}'".format(result[0]['id'],result[0]['Name'],result[0]['Tags'])
-                cursor.execute(sql)
-                connection.commit()
-
+                if  result[0]['Date'] < get_time_now():
+                    return 'old'
+                else:
+                    return 'new'
             else:
-                get_add_user(value)
-def get_add_user(value):
+                return False
+def get_show_news(value):
+    '''Show news  DataBase'''
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='telebot',
-                                 password='123321',
-                                 database='telebot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    with connection:
-        with connection.cursor() as cursor:
+    if get_check_news(value) == 'new':
 
-                insert = "INSERT INTO `Users` (`id`,`Name`,`Tags`) VALUES (%s, %s, %s)"
-                cursor.execute(insert, (value[0],f"{value[1]} {value[2]}",value[3]))
-                connection.commit()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `News` WHERE `Tags` = '{}'".format(value)
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                news = []
+                for i in result:
+                    news.append((i['News'], i['Link']))
+                    connection.commit()
+                return news
 
+    else:
+
+        with connection:
+            with connection.cursor() as cursor:
+                get_delete_news(value)
+                get_add_news(value)
+
+                sql = "SELECT * FROM `News` WHERE `Tags` = '{}'".format(value)
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                news = []
+                for i in result:
+                    news.append((i['News'], i['Link']))
+                    connection.commit()
+                return news
 def get_search_news(value):
     """ Поиск новостей из списка сайтов в файле 'search.json'"""
 
@@ -88,71 +102,124 @@ def get_search_news(value):
                 list_news.append([i.a.get_text(strip=True, separator= ' '), link + i.a.get('href'), value, get_time_now()])
 
     return list_news
-def add_news(value):
+def get_add_news(value):
     list = get_search_news(value)
 
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='telebot',
-                                 password='123321',
-                                 database='telebot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
     with connection:
         with connection.cursor() as cursor:
             #Isert new values
             for i in list:
                 insert = "INSERT INTO `News` (`News`,`Link`,`Tags`,`Date`) VALUES (%s, %s, %s, %s)"
                 cursor.execute(insert, (i[0],i[1],i[2],i[3]))
-                connection.commit()
-def get_show_news(value):
-    '''Show news  DataBase'''
-    news = []
-    connection = pymysql.connect(host='127.0.0.1',
-                                user='telebot',
-                                password='123321',
-                                database='telebot',
-                                charset='utf8mb4',
-                                cursorclass=pymysql.cursors.DictCursor)
-    with connection:
-        with connection.cursor() as cursor:
-            # Read all record
-            sql = "SELECT * FROM `News` WHERE `Tags` = '{}'".format(value)
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            for i in result:
-                news.append(i)
-                connection.commit()
-    return news
-def check_news(value):
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='telebot',
-                                 password='123321',
-                                 database='telebot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
+            connection.commit()
+def get_delete_news(value):
+
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
     with connection:
         with connection.cursor() as cursor:
-            # Read all record
-            sql = "SELECT `Date`,`Tags` FROM `News` WHERE `Tags` = '{}'".format(value)
+            sql = "DELETE FROM `News` WHERE `Tags` = '{}'".format(value)
             cursor.execute(sql)
-            result = cursor.fetchall()
-            if result:
-                if  result[0]['Date'] < get_time_now():
-                    sql = "DELETE FROM `News` WHERE `Tags` = '{}'".format(value)
-                    cursor.execute(sql)
-                    connection.commit()
-                    add_news(value)
-                    return get_show_news(value)
-                else:
-                    return get_show_news(value)
+            connection.commit()
 
 def get_time_now():
     # Текущая дата
     data = "{}-{}-{} {} hour ".format(str(datetime.datetime.now().year), str(datetime.datetime.now().month),
                               str(datetime.datetime.now().day), str(datetime.datetime.now().hour))
     return data
+
+def get_add_user(value):
+
+    value[1] = f"{value[1]} {value[2]}"
+
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+                insert = "INSERT INTO `Users` (`id`,`Name`,`Tags`) VALUES (%s, %s,%s)"
+                cursor.execute(insert, (value[0],value[1], ''))
+                connection.commit()
+def get_delete_user(value):
+
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+                delete = "DELETE FROM `Users` WHERE `id` = {}".format(value)
+                cursor.execute(delete)
+                connection.commit()
+def get_check_user(value):
+    id = value[0]
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `Users` WHERE `id` = '{}'".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            connection.commit()
+            if result:
+                return True
+            else:
+                return False
+
+def get_add_tags(value,user):
+
+
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+
+            if get_check_tags(value):
+                return False
+
+            else:
+                sql = "SELECT * FROM `Users` WHERE `id` = '{}'".format(user[0])
+                result = cursor.execute(sql)
+                result = cursor.fetchall()
+                if result:
+                    result[0]['Tags'] = f"{result[0]['Tags']} {value.lower()}"
+                    print(result)
+                    sql = "UPDATE `Users` SET `id`='{}',`Name`='{}',`Tags`='{}'".format(user[0],user[1],result[0]['Tags'])
+                    cursor.execute(sql)
+                    connection.commit()
+def get_check_tags(value, user):
+
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `Users` WHERE `id` = '{}'".format(user[0])
+            result = cursor.execute(sql)
+            result = cursor.fetchall()
+            if value in result[0]['Tags']:
+                return True
+            else:
+                return False
+def get_delete_tags(value,user):
+
+    connection = pymysql.connect(host='127.0.0.1', user='telebot', password='123321', database='telebot',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `Users` WHERE `id` = '{}'".format(user[0])
+            result = cursor.execute(sql)
+            result = cursor.fetchall()
+            result[0]['Tags'] = result[0]['Tags'].replace(value,"")
+            sql = "UPDATE `Users` SET `id`='{}',`Name`='{}',`Tags`='{}'".format(user[0], user[1], result[0]['Tags'])
+            cursor.execute(sql)
+            connection.commit()
 
 
 
@@ -167,19 +234,41 @@ def start(message):
     btn2 = types.KeyboardButton("✅ Subscribe")
     btn3 = types.KeyboardButton("🚫 Unsubscribe")
     markup.add(btn1, btn2, btn3)
-    text = f"""Hello {message.from_user.first_name} {message.from_user.last_name}!!! 
+    text = f"""Hello {message.from_user.first_name} {message.from_user.last_name}!!!
                \nI am a news bot that will help you learn the latest news in the world."""
     bot.send_message(message.chat.id, text, parse_mode='html',reply_markup=markup)
 
-
-
 @bot.message_handler()
 def get_user_text(message):
-    user = [message.from_user.id, message.from_user.first_name, message.from_user.last_name, message.text ]
-    #get_add_user(user)
+
     if (message.text == "📰 Top news"):
-        bot.send_message(message.chat.id , 'Введите тег новости')
-        bot.send_message(message.chat.id, get_show_news('helicopter'))
+        for news in get_show_news('Ukraine'):
+            text = f"<b>News:  </b>{news[0]}\n" + '<a href="{}">Source</a>'.format(news[1])
+            bot.send_message(message.chat.id ,text, parse_mode="HTML")
+
+    elif (message.text == "✅ Subscribe"):
+        user = [message.from_user.id, message.from_user.first_name, message.from_user.last_name]
+
+        if get_check_user(user):
+            bot.send_message(message.chat.id, 'You are already subscribed', parse_mode='html')
+        else:
+            get_add_user(user)
+            print(user)
+            bot.send_message(message.chat.id , 'Subscription completed successfully', parse_mode='html')
+            bot.send_message(message.chat.id, 'Enter the tag for which you want to receive news', parse_mode='html')
+
+    elif (message.text == "🚫 Unsubscribe"):
+
+        user = [message.from_user.id, message.from_user.first_name, message.from_user.last_name]
+
+        if get_check_user(user):
+
+            user = message.from_user.id
+            get_delete_user(user)
+            bot.send_message(message.chat.id, '🚫 You have unsubscribed from the newsletter ', parse_mode='html')
+
+        else:
+            bot.send_message(message.chat.id, 'You are not subscribed', parse_mode='html')
 
 bot.polling(none_stop=True)
 
--- app/Main.py
+++ app/Main.py
@@ -7,135 +7,61 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import datetime
+from datetime import datetime
 import pandas as pd
 import telebot
 
 def get_search_news(value):
-    """ Поиск новостей из списка сайтов в файле 'search.json'"""
 
-    #открытие файла
+    value = value.title()
+    list_news = []
+
     file = open('search.json', 'r')
     filer = file.read()
     dict_news = json.loads(filer)
 
-    list_news = []
+    # Установка аргументов для Chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
 
-    # Перебор сайтов , тегов
+    webdriver_service = Service("chromedriver/chromedriver") ## path to where you saved chromedriver binary
+    browser = webdriver.Chrome(service=webdriver_service,options=chrome_options)
+    #{'site': 'https://www.bbc.co.uk/search?q={}&d=HOMEPAGE_GNL',
+    # 'text': 'divclass_=ssrcss-1mb7gc4-PromoSwitchLayoutAtBreakpoints e3z3r3u0',
+    # 'date': 'spanclass_=ssrcss-1if1g9v-MetadataText ecn1o5v1', 'formatdate': '%d %B %Y'}
+
     for news in dict_news:
-        # Установка аргументов для Chrome
-        chrome_options = Options()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-
-
-        webdriver_service = Service("chromedriver/chromedriver") ## path to where you saved chromedriver binary
-        browser = webdriver.Chrome(service=webdriver_service,options=chrome_options)
 
         # Проверка кол-ва вводимых слов
         if len(value.split()) > 1:
-            browser.get(news.format('%20'.join(value.split())))
+            browser.get(news['site'].format('%20'.join(value.split())))
         else:
-            browser.get(news.format(value))
+            browser.get(news['site'].format(value))
 
         soup = bs(browser.page_source, 'lxml')
-        browser.quit()
-        tags = dict_news[news].split('class_=')
-        res = soup.find_all(tags[0], class_= tags[1], limit= 2)
+
+        # browser.quit()
+        tags = news['text'].split('class_=')
+        datatags = news['date'].split('class_=')
+        res = soup.find_all(tags[0], class_=tags[1], limit=3)
 
         for i in res:
+
+            data = i.find(datatags[0], class_=datatags[1]).get_text(strip=True, separator= ' ')
+            data = datetime.strptime(data,news['formatdate']).strftime('%Y-%m-%d')
+
             if 'http' in i.a.get('href'):
-                list_news.append([i.a.get_text(strip=True, separator= ' '),i.a.get('href'), value, get_time_now()])
+                list_news.append([i.a.get_text(strip=True, separator= ' '),i.a.get('href'), value,data])
             else:
-                link = re.match('^h.*\.com', news ).group()
-                list_news.append([i.a.get_text(strip=True, separator= ' '), link + i.a.get('href'), value, get_time_now()])
+                link = re.match('^h.*\.(com|uk|org)', news["site"] ).group()
+                print(link)
+                list_news.append([i.a.get_text(strip=True, separator= ' '), link + i.a.get('href'), value,data])
 
     return list_news
 
-def get_add_news(value):
-    list = get_search_news(value)
-
-
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='telebot',
-                                 password='123321',
-                                 database='telebot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    with connection:
-        with connection.cursor() as cursor:
-            #Isert new values
-            for i in list:
-                insert = "INSERT INTO `News` (`News`,`Link`,`Tags`,`Date`) VALUES (%s, %s, %s, %s)"
-                cursor.execute(insert, (i[0],i[1],i[2],i[3]))
-                connection.commit()
-
-def get_show_news(value):
-    '''Show news  DataBase'''
-    news = []
-    connection = pymysql.connect(host='127.0.0.1',
-                                user='telebot',
-                                password='123321',
-                                database='telebot',
-                                charset='utf8mb4',
-                                cursorclass=pymysql.cursors.DictCursor)
-    with connection:
-        with connection.cursor() as cursor:
-            # Read all record
-            sql = "SELECT * FROM `News` WHERE `Tags` = '{}'".format(value)
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            for i in result:
-                news.append(i)
-                connection.commit()
-    return news
-
-def get_time_now():
-    # Текущая дата
-    data = "{}-{}-{} {} hour ".format(str(datetime.datetime.now().year), str(datetime.datetime.now().month),
-                              str(datetime.datetime.now().day), str(datetime.datetime.now().hour))
-    return data
-
-def check_news(value):
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='telebot',
-                                 password='123321',
-                                 database='telebot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-
-    with connection:
-        with connection.cursor() as cursor:
-            # Read all record
-            sql = "SELECT `Date`,`Tags` FROM `News` WHERE `Tags` = '{}'".format(value)
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            if result:
-                if  result[0]['Date'] < get_time_now():
-                    sql = "DELETE FROM `News` WHERE `Tags` = '{}'".format(value)
-                    cursor.execute(sql)
-                    connection.commit()
-                    get_add_news(value)
-                    return get_show_news(value)
-                else:
-                    return get_show_news(value)
-
-def get_add_user(value):
-    value = ['346488140' , 'Alexandr Brovchenko', 'Poland']
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='telebot',
-                                 password='123321',
-                                 database='telebot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    with connection:
-        with connection.cursor() as cursor:
-                insert = "INSERT INTO `Users` (`id`,`Name`,`Tags`) VALUES (%s, %s, %s)"
-                cursor.execute(insert, (value[0],value[1],value[2]))
-                connection.commit()
 
 
 
+print(get_search_news('ukraine'))
 
-
-print(get_add_news('Panama'))
