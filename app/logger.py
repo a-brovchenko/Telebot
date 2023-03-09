196aa8c7bd476a9266ad110ebf90ea264fa05982
Alexander Brovchenko <a.brovchenko@unix4all.com>
2023-03-09 17:12:29 +0200

    Logging file path

--- app/logger.py
+++ app/logger.py
@@ -2,7 +2,7 @@ import logging
 from pythonjsonlogger import jsonlogger
 
 formatter = jsonlogger.JsonFormatter("%(asctime)s %(threadName)s %(name)s %(levelname)s %(message)s")
-file_out = logging.FileHandler('/var/log/telebot.log')
+file_out = logging.FileHandler('/app/telebot.log')
 console_out = logging.StreamHandler()
 file_out.setFormatter(formatter)
 console_out.setFormatter(formatter)
